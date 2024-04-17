db208961d3810784f9f2fb2713e750463fc3ea42
Z24B <zivha.beharie@gmail.com>
2024-04-17 03:36:08 -0700

    Task 8

--- models/city.py
+++ models/city.py
@@ -8,11 +8,7 @@ import os
 
 class City(BaseModel, Base):
     """ The city class, contains state ID and name """
-    __tablename__ = 'cities'
-    if os.getenv('HBNB_TYPE_STORAGE') == 'db':
-        state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
-        name = Column(String(128), nullable=False)
-        places = relationship("Place", passive_deletes=True, backref="cities")
-    else:
-        state_id = ""
-        name = ""
+    __tablename__ = "cities"
+    name = Column(String(128), nullable=False)
+    state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+    places = relationship("Place", backref="cities", cascade="all, delete")
--- models/user.py
+++ models/user.py
@@ -1,25 +1,18 @@
 #!/usr/bin/python3
 """This module defines a class User"""
 from models.base_model import BaseModel, Base
-from sqlalchemy import String, Column
+from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
-import os
+from models.review import Review
+from models.place import Place
 
 
 class User(BaseModel, Base):
-    '''
-        Definition of the User class
-    '''
+    """This class defines a user by various attributes"""
     __tablename__ = "users"
-    if os.getenv('HBNB_TYPE_STORAGE') == 'db':
-        email = Column(String(128), nullable=False)
-        password = Column(String(128), nullable=False)
-        first_name = Column(String(128), nullable=True)
-        last_name = Column(String(128), nullable=True)
-        places = relationship("Place", passive_deletes=True, backref="user")
-        reviews = relationship("Review", passive_deletes=True, backref="user")
-    else:
-        email = ""
-        password = ""
-        first_name = ""
-        last_name = ""
+    email = Column(String(128), nullable=False)
+    password = Column(String(128), nullable=False)
+    first_name = Column(String(128), nullable=False)
+    last_name = Column(String(128), nullable=False)
+    places = relationship("Place", backref="user", cascade="all, delete")
+    reviews = relationship("Review", backref="user", cascade="all, delete")
