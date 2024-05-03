c18c9ff264451c68249e6fc44fe8d90596d3c376
Z24B <zivha.beharie@gmail.com>
2024-05-03 04:50:37 -0700

    Task 2

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """Fabric script that distributes an archive to your web servers"""
 from fabric.api import env, put, run
-import os
+import os.path
 
 env.hosts = ['100.26.240.110', '54.157.160.220']
 env.user = 'ubuntu'
@@ -9,36 +9,24 @@ env.key_filename = ['/root/.ssh/id_rsa']
 
 
 def do_deploy(archive_path):
-    """Distributes an archive to your web servers and deploys it"""
+    """Distributes an archive to web servers"""
     if not os.path.exists(archive_path):
         return False
+    file_name = archive_path.split('/')[1]
+    file_path = '/data/web_static/releases/'
+    releases_path = file_path + file_name[:-4]
 
     try:
         # Upload the archive to the /tmp/ directory of the web server
         put(archive_path, '/tmp/')
-        filename = os.path.basename(archive_path)
-        folder_name = filename.split('.')[0]
-        run('mkdir -p /data/web_static/releases/{}/'.format(folder_name))
-        run('tar -xzf /tmp/{} -C /data/web_static/releases/{}/'.format(
-            filename, folder_name))
-
-        # Delete the archive from the web server
-        run('rm /tmp/{}'.format(filename))
-
-        # Delete the symbolic link /data/web_static/current from the web server
+        run('mkdir -p {}'.format(releases_path))
+        run('tar -xzf /tmp/{} -C {}'.format(file_name, releases_path))
+        run('rm /tmp/{}'.format(file_name))
+        run('mv {}/web_static/* {}/'.format(releases_path, releases_path))
+        run('rm -rf {}/web_static'.format(releases_path))
         run('rm -rf /data/web_static/current')
-
-        # Create new link /data/web_static/current linked to new version
-        run('ln -s /data/web_static/releases/{}/ '
-            '/data/web_static/current'.format(folder_name))
-
-        print("New version deployed!")
+        run('ln -s {} /data/web_static/current'.format(releases_path))
+        print('New version deployed!')
         return True
-
-    except Exception as e:
-        print(e)
+    except:
         return False
-
-
-if __name__ == "__main__":
-    pass
