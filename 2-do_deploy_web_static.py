77a54227f1287dbcb03298bca0769df8b397d748
Z24B <zivha.beharie@gmail.com>
2024-05-03 04:01:11 -0700

    Task 2

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -19,7 +19,8 @@ def do_deploy(archive_path):
         filename = os.path.basename(archive_path)
         folder_name = filename.split('.')[0]
         run('mkdir -p /data/web_static/releases/{}/'.format(folder_name))
-        run('tar -xzf /tmp/{} -C /data/web_static/releases/{}/'.format(filename, folder_name))
+        run('tar -xzf /tmp/{} -C /data/web_static/releases/{}/'.format(
+            filename, folder_name))
 
         # Delete the archive from the web server
         run('rm /tmp/{}'.format(filename))
@@ -27,8 +28,9 @@ def do_deploy(archive_path):
         # Delete the symbolic link /data/web_static/current from the web server
         run('rm -rf /data/web_static/current')
 
-        # Create a new symbolic link /data/web_static/current linked to the new version
-        run('ln -s /data/web_static/releases/{}/ /data/web_static/current'.format(folder_name))
+        # Create new link /data/web_static/current linked to new version
+        run('ln -s /data/web_static/releases/{}/ '
+            '/data/web_static/current'.format(folder_name))
 
         print("New version deployed!")
         return True
