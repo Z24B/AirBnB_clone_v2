8d1890941de8fccf702995dad5ca93727fff4155
Z24B <zivha.beharie@gmail.com>
2024-05-03 00:27:37 -0700

    Task 1

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -11,13 +11,21 @@ import os.path
 def do_pack():
     """Generates a .tgz archive from the contents of the web_static folder"""
     try:
-        date = datetime.now().strftime("%Y%m%d%H%M%S")
-        if isdir("versions") is False:
-            local("mkdir versions")
+        # Create the versions directory if it doesn't exist
+        local("mkdir -p versions")
 
-        # Generate archive name 
-        file_name = "versions/web_static_{}.tgz".format(date)
-        local("tar -cvzf {} web_static".format(file_name))
-        return file_name
-    except:
+        # Generate archive name
+        now = datetime.now()
+        archive_name = "web_static_{}{}{}{}{}{}.tgz".format(
+                now.year, now.month, now.day, now.hour, now.minute, now.second)
+
+        # Compress web_static folder into archive
+        local("tar -cvzf versions/{} web_static".format(archive_name))
+
+        # Return archive path if successfully generated
+        return "versions/{}".format(archive_name)
+    except Exception:
         return None
+
+if __name__ == "__main__":
+    do_pack()
