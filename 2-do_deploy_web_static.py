6b0641733983129641f05238d083268cf8ded6e0
Z24B <zivha.beharie@gmail.com>
2024-05-03 03:48:10 -0700

    Task 2

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,35 +1,42 @@
 #!/usr/bin/python3
 """Fabric script that distributes an archive to your web servers"""
-from fabric.api import env, run, put, sudo, local
-from datetime import datetime
-from os import path
+from fabric.api import env, put, run
+import os
 
 env.hosts = ['100.26.240.110', '54.157.160.220']
 env.user = 'ubuntu'
-env.key_filename = '/root/.ssh/id_rsa'
+env.key_filename = ['/root/.ssh/id_rsa']
 
 
 def do_deploy(archive_path):
-    """Distributes an archive to web servers"""
-    if not path.exists(archive_path):
+    """Distributes an archive to your web servers and deploys it"""
+    if not os.path.exists(archive_path):
         return False
+
     try:
-        put(archive_path, "/tmp/")
-        name = archive_path.split('/')[1].split('.')[0]
-        sudo("mkdir -p /data/web_static/releases/{}/".format(name))
-        sudo("tar -xzf /tmp/{}.tgz -C /data/web_static/releases/{}/"
-             .format(name, name))
-        run("rm /tmp/{}.tgz".format(name))
-        sudo("mv /data/web_static/releases/{}/web_static/*\
-             /data/web_static/releases/{}/".format(name, name))
-        sudo("rm -rf /data/web_static/releases/{}/web_static".format(name))
-        sudo("rm -rf /data/web_static/current")
-        sudo("ln -s /data/web_static/releases/{}/ /data/web_static/current"
-             .format(name))
+        # Upload the archive to the /tmp/ directory of the web server
+        put(archive_path, '/tmp/')
+        filename = os.path.basename(archive_path)
+        folder_name = filename.split('.')[0]
+        run('mkdir -p /data/web_static/releases/{}/'.format(folder_name))
+        run('tar -xzf /tmp/{} -C /data/web_static/releases/{}/'.format(filename, folder_name))
+
+        # Delete the archive from the web server
+        run('rm /tmp/{}'.format(filename))
+
+        # Delete the symbolic link /data/web_static/current from the web server
+        run('rm -rf /data/web_static/current')
+
+        # Create a new symbolic link /data/web_static/current linked to the new version
+        run('ln -s /data/web_static/releases/{}/ /data/web_static/current'.format(folder_name))
+
+        print("New version deployed!")
         return True
-    except Exception:
+
+    except Exception as e:
+        print(e)
         return False
 
 
 if __name__ == "__main__":
-    do_deploy()
+    pass
