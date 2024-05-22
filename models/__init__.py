30d3a019904e166aaa671cfbd30ff0b2d1bc85be
Z24B <zivha.beharie@gmail.com>
2024-05-22 03:51:44 -0700

    Task Complete

--- models/__init__.py
+++ models/__init__.py
@@ -1,6 +1,15 @@
 #!/usr/bin/python3
 """Initializes the models package"""
 from os import getenv
+from models.engine.file_storage import FileStorage
+from models.engine.db_storage import DBStorage
+from models.base_model import BaseModel
+from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
 
 STORAGE_TYPE = getenv('HBNB_TYPE_STORAGE')
 
--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """ """
 from tests.test_models.test_base_model import test_basemodel
-from models.amenity import Amenity
+from models
 
 
 class test_Amenity(test_basemodel):
